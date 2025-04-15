5195ae4f18272c2b2340d3c65512d27217650b6e
KunalChaprana0011 <kunalchaprana8093@gmail.com>
2025-04-15 10:29:34 +0530

    setting up our Convex database  and defining the Schemas

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,18 @@
-
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import { tokenCache } from '@clerk/clerk-expo/token-cache'
-import { ClerkProvider } from "@clerk/clerk-expo";
+
 import { StatusBar } from "react-native";
 import Initiallayout from "@/components/Initiallayout";
+import ClerkAndConvexProvider from "@/providers/ClerkAndConvexProvider";
 
 export default function RootLayout() {
   return (
-    <ClerkProvider tokenCache={tokenCache}>
+    <ClerkAndConvexProvider>
       <SafeAreaProvider>
-      <StatusBar backgroundColor="black" barStyle="light-content" />
+        <StatusBar backgroundColor="black" barStyle="light-content" />
         <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
           <Initiallayout />
         </SafeAreaView>
       </SafeAreaProvider>
-    </ClerkProvider>
+    </ClerkAndConvexProvider>
   );
 }
---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+// https://working-mayfly-65.clerk.accounts.dev
+
+export default {
+    providers: [
+      {
+        domain: "https://working-mayfly-65.clerk.accounts.dev/",
+        applicationID: "convex",
+      },
+    ]
+  };
